c546e209b7c4b5c9dae882b35edc931e196647bc
Beambear <daidaixiong119@gmail.com>
2022-06-13 17:49:05 -0400

    498 done

--- src/driver/Main.java
+++ src/driver/Main.java
@@ -1,21 +1,31 @@
 package driver;
 
 import google.DriverGoogle;
+import practice.FindDiagonalOrder;
 import practice.HeightChecker;
+
+import java.lang.reflect.Array;
+import java.util.Arrays;
 
 public class Main {
 
     public static void main(String[] args) {
 	    Main driver = new Main();
-        driver.practice(1051);
+        driver.practice(498);
     }
 
     public void practice(int practiceNum){
         switch(practiceNum){
+            case 498 :
+                FindDiagonalOrder doIt498 = new FindDiagonalOrder();
+                int[] test498 = doIt498.findDiagonalOrder(new int[][]{{2,3}});
+                System.out.println("test 498: "+ Arrays.toString(test498));
+                break;
+
             case 1051:
-                HeightChecker doIt = new HeightChecker();
-                int result=doIt.heightChecker(new int[]{1, 1, 4, 2, 1, 3});
-                System.out.println(result);
+                HeightChecker doIt1051 = new HeightChecker();
+                int test1051=doIt1051.heightChecker(new int[]{1, 1, 4, 2, 1, 3});
+                System.out.println("test 1051: "+test1051);
                 break;
         }
     }
---
+++ src/practice/FindDiagonalOrder.java
@@ -0,0 +1,42 @@
+package practice;
+//给你一个大小为 m x n 的矩阵 mat ，请以对角线遍历的顺序，用一个数组返回这个矩阵中的所有元素。
+public class FindDiagonalOrder {
+
+    // x+y = n
+    // n = 2,3,4,5......2*x
+    public int[] findDiagonalOrder(int[][] mat){
+        int[] array = new int[mat[0].length*mat.length];
+        int count=0;
+        int maxN=0;
+        if(mat[0].length > mat.length){
+            maxN=2*mat[0].length-2;
+        }else{
+            maxN=2*mat.length-2;
+        }
+        System.out.println("max N:"+(2*mat.length-2));
+        for(int n=0;n<=maxN;n++){
+            if(n%2 == 0){   //left to right && bot to top
+                for(int x = 0; x <=n;x++){
+                    int y=n-x;
+                    if(x<mat[0].length && y<mat.length){
+                        array[count]=mat[y][x];
+                        count++;
+                        System.out.println("Curr N:"+n+"; x:"+x+"; y:"+y);
+                    }
+                }
+            }
+            if(n%2 == 1){   //right to left && top to bot
+                for(int y = 0; y<=n;y++){
+                    int x=n-y;
+                    if(x<mat[0].length && y<mat.length){
+                        array[count]=mat[y][x];
+                        count++;
+                        System.out.println("Curr N:"+n+"; x:"+x+"; y:"+y);
+                    }
+                }
+            }
+
+        }
+        return array;
+    }
+}
